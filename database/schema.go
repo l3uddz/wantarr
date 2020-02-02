0d756db787f752823578d7a9c0ad3e7cf2d9fa17
l3uddz <l3uddz@gmail.com>
2020-02-02 20:03:50 +0000

    change(database): dont auto increment id (fixes int and composite key issue)

--- database/schema.go
+++ database/schema.go
@@ -3,7 +3,7 @@ package database
 import "time"
 
 type MediaItem struct {
-	Id                int    `gorm:"primary_key"`
+	Id                int    `gorm:"primary_key;auto_increment:false"`
 	PvrName           string `gorm:"primary_key"`
 	WantedType        string `gorm:"primary_key"`
 	AirDateUtc        time.Time
