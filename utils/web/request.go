f7531b58402537659bbda61dac0e58c3722ac9a1
l3uddz <l3uddz@gmail.com>
2020-02-18 23:34:31 +0000

    web: dont use req default timeout

--- utils/web/request.go
+++ utils/web/request.go
@@ -45,7 +45,10 @@ const (
 /* Private */
 
 func init() {
+	// dont json escape html
 	req.SetJSONEscapeHTML(false)
+	// use timeout from context
+	req.SetTimeout(0)
 }
 
 func getInputsWithTimeout(inputs []interface{}, timeout int) []interface{} {
